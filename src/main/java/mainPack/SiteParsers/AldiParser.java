37d4cd57b871a363cddf5837c624479bf6d65680
Valery Oster <valera_oster@hotmail.com>
2017-03-13 13:05:21 +0100

    *Update AldiParsing

--- src/main/java/mainPack/SiteParsers/AldiParser.java
+++ src/main/java/mainPack/SiteParsers/AldiParser.java
@@ -58,14 +58,49 @@ public class AldiParser implements ParserAll {
     private void getAllOffersByUrl(String url) {
         Document doc;
         System.out.println(url);
-
-
+        String offersName = "";
+        String offersPrice = "";
+        String offersManuf = "";
+        String offersProp = "";
+        String offersImgUrl = "";
         try {
             doc = Jsoup.connect(url).get();
             Elements elements1 = doc.getElementsByClass("offer");
-            Elements elements2 = doc.getElementsByClass("offer");
+            Elements elements2 = doc.getElementsByClass("product-tile");
 
+            int i = 1;
+            System.out.println("****************ELEMENTS Offer***************");
             for (Element element : elements1) {
+
+                //Erste Element ist zerteilt worden, in einem liegt Bild in anderem Info
+                if (element.getElementsByClass("detaillink").first() != null) {
+
+                    offersName = element.getElementsByClass("detaillink").first().text();
+                    offersPrice = element.getElementsByClass("main clearfix").first().
+                                getElementsByTag("strong").first().text().replace("*","");
+
+                }else {
+                    Element el = element.parent().getElementsByClass("offer-1-content").first();
+
+                    offersName = el.getElementsByClass("detaillink").first().text();
+                    offersPrice = el.getElementsByClass("main clearfix").first().
+                            getElementsByTag("strong").first().text().replace("*","");
+                }
+
+                System.out.println(i++ +" ----------------------");
+                System.out.println("Name: " + offersName);
+                System.out.println("Price: " + offersPrice);
+                System.out.println("------------------------");
+            }
+            System.out.println("**************ELEMENTS product-tile Red****************");
+            for (Element element : elements2) {
+                if (element.getElementsByClass("detaillink").first() != null) {
+
+                    offersName = element.getElementsByClass("detaillink").first().text();
+                }else {
+
+                }
+                System.out.println(i++ +" ----------------------");
                 System.out.println(element.text());
                 System.out.println("------------------------");
             }
